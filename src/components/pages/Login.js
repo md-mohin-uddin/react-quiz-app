03d4a3d84edb5cf0ddd59cd36a6c4ac32b5570e0
Your Name <you@example.com>
2023-12-07 20:55:57 +0600

    source code lesson-10

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,37 +1,15 @@
-import React from "react";
 import Illustration from "../Illustration";
-import Form from "../Form";
-import TextInput from "../TextInput";
-import Button from "../Button";
-import classes from "../../styles/Login.module.css";
-import { Link } from "react-router-dom";
+import LoginForm from "../LoginForm";
 
-export default function Login() {
+export default function Signup() {
   return (
-    <div>
+    <>
       <h1>Login to your account</h1>
+
       <div className="column">
         <Illustration />
-        <Form className={`{${classes.login}}`}>
-          <TextInput
-            type="text"
-            placeholder="Enter email"
-            icon="alternate_email"
-          />
-          <TextInput
-            type="password "
-            placeholder="Enter password"
-            icon="lock"
-          />
-
-          <Button>
-            <span>Submit now</span>
-          </Button>
-          <div className="info">
-            Don't have an account? <Link to="/signup">Signup</Link> instead.
-          </div>
-        </Form>
+        <LoginForm />
       </div>
-    </div>
+    </>
   );
 }
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,47 +1,15 @@
-import React from "react";
-import Form from "../Form";
 import Illustration from "../Illustration";
-import TextInput from "../TextInput";
-import classes from "../../styles/Signup.module.css";
-import Checkbox from "../Checkbox";
-import Button from "../Button";
-import { Link } from "react-router-dom";
+import SignupForm from "../SignupForm";
 
 export default function Signup() {
   return (
-    <div>
-      <>
-        <h1>Create an account</h1>
-        <div className="column">
-          <Illustration />
-          <Form className={`{${classes.signup}}`}>
-            <TextInput type="text" placeholder="Enter name" icon="person" />
-            <TextInput
-              type="text"
-              placeholder="Enter email"
-              icon="alternate_email"
-            />
-            <TextInput
-              type="password "
-              placeholder="Enter password"
-              icon="lock"
-            />
-            <TextInput
-              type="password "
-              placeholder="Confirm password"
-              icon="lock_clock"
-            />
+    <>
+      <h1>Create an account</h1>
 
-            <Checkbox text="I agree to the Terms & Conditions" />
-            <Button>
-              <span>Submit now</span>
-            </Button>
-            <div className="info">
-              Already have an account? <Link to="/login">Login</Link> instead.
-            </div>
-          </Form>
-        </div>
-      </>
-    </div>
+      <div className="column">
+        <Illustration />
+        <SignupForm />
+      </div>
+    </>
   );
 }
